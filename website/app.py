1471d7d26422e816e160440877585af513ab8240
Rick Cavalla <monolith32@gmail.com>
2019-09-21 13:39:29 -0400

    Combined data json flask route

--- website/app.py
+++ website/app.py
@@ -31,6 +31,7 @@ Base.prepare(db.engine, reflect=True)
 # Save references to each table
 Candy = Base.classes.candy
 Diabetes = Base.classes.diabetes
+Combined = Base.classes.combined
 
 @app.route("/")
 def index():
@@ -65,69 +66,38 @@ def diabetes_rate():
 
     return diabetes_df.to_json()
 
-# @app.route("/names")
-# def names():
-#     """Return a list of sample names."""
+@app.route("/candy-diabetes-combined")
+def candy_diabetes_combined():
+    """Return diabetes rate and per capita consumption by state"""
 
-#     # Use Pandas to perform the sql query
-#     stmt = db.session.query(Samples).statement
-#     df = pd.read_sql_query(stmt, db.session.bind)
+    # # Use Pandas to perform the sql query
+    # candy_stmt = db.session.query(Candy).statement
+    # candy_df = pd.read_sql_query(candy_stmt, db.session.bind)
+    # candy_df = candy_df[
+    #     ["State",
+    #      "State_2",
+    #      "Per_Capita_Consumption_Pounds_Per_Person"]]
 
-#     # Return a list of the column names (sample names)
-#     return jsonify(list(df.columns)[2:])
+    # # Use Pandas to perform the sql query
+    # diabetes_stmt = db.session.query(Diabetes).statement
+    # diabetes_df = pd.read_sql_query(diabetes_stmt, db.session.bind)
+    # diabetes_df = diabetes_df[
+    #     ["State_2",
+    #      "Diabetes Rate 2018"]]
 
+    # combined_df = candy_df.merge(diabetes_df, on="State_2")
 
-# @app.route("/metadata/<sample>")
-# def sample_metadata(sample):
-#     """Return the MetaData for a given sample."""
-#     sel = [
-#         Samples_Metadata.sample,
-#         Samples_Metadata.ETHNICITY,
-#         Samples_Metadata.GENDER,
-#         Samples_Metadata.AGE,
-#         Samples_Metadata.LOCATION,
-#         Samples_Metadata.BBTYPE,
-#         Samples_Metadata.WFREQ,
-#     ]
+    combined_stmt = db.session.query(Combined).statement
+    combined_df = pd.read_sql_query(combined_stmt, db.session.bind)
+    combined_df['combined_pounds'] =\
+         combined_df['1st_Pounds']\
+         +  combined_df['2nd_Pounds']\
+         +  combined_df['3rd_Pounds']
+    combined_df['per_capita_consumption'] =\
+         combined_df.combined_pounds * 100 / combined_df.Population_2019
+    combined_df.to_json
 
-#     results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
-
-#     # Create a dictionary entry for each row of metadata information
-#     sample_metadata = {}
-#     for result in results:
-#         sample_metadata["sample"] = result[0]
-#         sample_metadata["ETHNICITY"] = result[1]
-#         sample_metadata["GENDER"] = result[2]
-#         sample_metadata["AGE"] = result[3]
-#         sample_metadata["LOCATION"] = result[4]
-#         sample_metadata["BBTYPE"] = result[5]
-#         sample_metadata["WFREQ"] = result[6]
-
-#     print(sample_metadata)
-#     return jsonify(sample_metadata)
-
-
-# @app.route("/samples/<sample>")
-# def samples(sample):
-#     """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-#     stmt = db.session.query(Samples).statement
-#     df = pd.read_sql_query(stmt, db.session.bind)
-
-#     # Filter the data based on the sample number and
-#     # only keep rows with values above 1
-#     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
-
-#     # Sort by sample
-#     sample_data.sort_values(by=sample, ascending=False, inplace=True)
-
-#     # Format the data to send as json
-#     data = {
-#         "otu_ids": sample_data.otu_id.values.tolist(),
-#         "sample_values": sample_data[sample].values.tolist(),
-#         "otu_labels": sample_data.otu_label.tolist(),
-#     }
-#     return jsonify(data)
-
+    return combined_df.to_json()
 
 if __name__ == "__main__":
     app.run()
